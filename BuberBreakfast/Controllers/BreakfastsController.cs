704dd6c4b5f157c8c61a55eae135db9e6f983f45
Your Name <you@example.com>
2024-01-31 00:29:35 +0800

    windows

--- BuberBreakfast/Controllers/BreakfastsController.cs
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -1,14 +1,21 @@
 using BuberBreakfast.Contracts.Breakfast;
 using BuberBreakfast.Models;
+using BuberBreakfast.Services.Breakfasts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberBreakfast.Controllers;
 
 [ApiController]
-[Route("breakfasts")]
+[Route("[controller]")]
 public class BreakfastsController: ControllerBase
 {
-    [HttpPost()]
+    private readonly IBreakfastService _breakfastService;
+
+    public BreakfastsController(IBreakfastService breakfastService)
+    {
+        _breakfastService = breakfastService;
+    }
+    [HttpPost]
     public IActionResult CreateBreakfast(CreateBreakfastRequest request)
     {
         var breakfast = new Breakfast(
@@ -23,6 +30,7 @@ public class BreakfastsController: ControllerBase
         );    //mapping the data from the request
         
             
+        _breakfastService.CreateBreakfast(breakfast);
 
         var response = new BreakfastResponse(
             breakfast.Id,
@@ -44,7 +52,18 @@ public class BreakfastsController: ControllerBase
     [HttpGet("{id:guid}")]
     public IActionResult GetBreakfast(Guid id)
     {
-        return Ok(id);
+        Breakfast breakfast = _breakfastService.GetBreakfast(id);
+        var response = new BreakfastResponse(
+            breakfast.Id,
+            breakfast.Name,
+            breakfast.Description,
+            breakfast.StartDateTime,
+            breakfast.EndDateTime,
+            breakfast.LastModifiedDataTime,
+            breakfast.Savory,
+            breakfast.Sweet
+        );
+        return Ok(response);
     }
     [HttpPut("{id:guid}")]
     public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
